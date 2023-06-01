41ac3e1c828673437e0da0ddd75e0d6528c85460
Eugene Clark <clarkjr2016@gmail.com>
2023-06-01 17:29:14 -0400

    I leveraged webpack —watch and liveserver to be able see my code updates automatically

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -12,6 +16,13 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext]",
+        },
       },
     ],
   },
